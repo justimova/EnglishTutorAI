9740d5d78b92784a9647d24152d24b6e3f10e7c7
L.Ustimova <l.ustimova@creatio.com>
2023-11-26 23:14:14 +0100

    #2 Create solution (ASP NET Core + angular)

---
+++ EnglishTutorAI/Program.cs
@@ -0,0 +1,8 @@
+using EnglishTutorAI;
+
+var builder = Host.CreateDefaultBuilder(args)
+	.ConfigureWebHostDefaults(webBuilder => {
+		webBuilder.UseStartup<Startup>();
+	});
+var app = builder.Build();
+app.Run();
---
+++ EnglishTutorAI/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+
+namespace EnglishTutorAI
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/dist";
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseStaticFiles();
+			if (!env.IsDevelopment())
+			{
+				app.UseSpaStaticFiles();
+			}
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
+
+				if (env.IsDevelopment())
+				{
+					spa.UseAngularCliServer(npmScript: "start");
+				}
+			});
+		}
+	}
+}
