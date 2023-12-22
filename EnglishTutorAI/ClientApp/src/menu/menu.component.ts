c264450e413c6a7bf44bbeaac48cb80b96fcf8d3
justimova <vyhhlz@gmail.com>
2023-12-22 02:09:47 +0100

    #7 Create home page and main menu

--- EnglishTutorAI/ClientApp/src/menu/menu.component.ts
+++ EnglishTutorAI/ClientApp/src/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuItem } from '../_interfaces/menu/menu-item';
 @Component({
   selector: 'app-menu',
   //standalone: true,
@@ -9,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
   isCollapsed: boolean = false;
   public nameCompany: string = "English AI Tutor";
-  
+  menuItems: MenuItem[] = [{
+    name: 'Dashboard',
+    route: '/dashboard',
+    isActive: true
+  }, {
+    name: 'Grammar',
+    route: '/grammar',
+    isActive: false
+  }, {
+    name: 'Reading',
+    route: '/reading',
+    isActive: false
+  }, {
+    name: 'Writing',
+    route: '/writing',
+    isActive: false
+  }, {
+    name: 'My Profile',
+    route: '/profile',
+    isActive: false
+  }];
+
   ngOnInit(): void {
   }
 
